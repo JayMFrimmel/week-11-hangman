789bf8a86d4eecf565342b66e9d0222924c0f3b6
JayMFrimmel <jayfrimmel@yahoo.com>
2016-09-30 15:16:06 -0400

    added letters and words

---
+++ game.js
@@ -0,0 +1,5 @@
+//require inquirer
+var inquirer = require('inquirer');
+//create a list of leters to choose from
+var availableLetters = "abcdefghijklmnopqrstuvwxyz";
+var words = ["pumpkin", "ghost", "witch", "skeleton", "zombie"];
